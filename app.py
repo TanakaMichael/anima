b4c8cdc4c9e4f76910a1cf1617e9a372e8772955
TanakaMichael <yuusya12411@outlook.jp>
2024-08-25 17:51:58 +0900

    add text

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test update.'
+    return 'Hello, World! This is a test update. os ubuntu, app flask, '
 
 if __name__ == '__main__':
     app.run(debug=True)
