04b1a4ac8dc4b2b33d83b37c69b6e7076fc1f119
TanakaMichael <yuusya12411@outlook.jp>
2024-08-25 17:59:43 +0900

    add text siraga

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test update. os ubuntu, app flask and Python!'
+    return 'Hello, World! This is a test update. os ubuntu, app flask and Python! エラーが起こるたびに白髪が増える...'
 
 if __name__ == '__main__':
     app.run(debug=True)
