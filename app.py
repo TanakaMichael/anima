a9710bfa0d5001580017ef08c4d0f929808a1579
Ubuntu <ubuntu@ip-172-26-7-49.ap-northeast-1.compute.internal>
2024-08-26 04:58:27 +0000

    Initial commit of local project

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
+# app.py
 from flask import Flask
-
 
 app = Flask(__name__)
 
-
 @app.route('/')
-
-def hello():
-
-    return "Hello, World!"
-
+def hello_world():
+    return 'Hello, World! This is a test update. os ubuntu, app flask and Python! 何が問題なのかわからない...多分動いてる動いて'
 
 if __name__ == '__main__':
-
-    app.run()
-
+    app.run(debug=True)
