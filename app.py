bfcada8405ed8bf73101bd7939c8c183eb599cab
TanakaMichael <yuusya12411@outlook.jp>
2024-08-26 11:45:12 +0900

    test commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test update. os ubuntu, app flask and Python! 何が問題なのかわからない...多分動いてる'
+    return 'Hello, World! This is a test update. os ubuntu, app flask and Python! 何が問題なのかわからない...多分動いてる動いて'
 
 if __name__ == '__main__':
     app.run(debug=True)
