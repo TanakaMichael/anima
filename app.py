a3947a49afbe3423191c73d2922424b4b7dfe31b
TanakaMichael <yuusya12411@outlook.jp>
2024-08-26 15:45:16 +0900

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-# app.py
-from flask import Flask, render_template, request, jsonify
-import os
 import subprocess
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -21,16 +19,20 @@ def webhook():
     if data.get('ref') == 'refs/heads/master':
         try:
             # Gitリポジトリを最新に更新
-            subprocess.run(['/usr/bin/git', 'pull'], cwd='/home/ubuntu/myflaskapp', check=True)
+            result = subprocess.run(['/usr/bin/git', 'pull'], cwd='/home/ubuntu/myflaskapp', check=True, capture_output=True, text=True)
+            print(f"Git pull output: {result.stdout}")
+            print(f"Git pull error: {result.stderr}")
             # Flaskアプリケーションを再起動
             subprocess.run(['sudo', 'systemctl', 'restart', 'myflaskapp'], check=True)
             return "Success", 200
         except subprocess.CalledProcessError as e:
             # もしGit pullやアプリケーションの再起動に失敗した場合、エラーメッセージを返す
             print(f"Error during webhook processing: {e}")
+            print(f"Git pull output: {e.output}")
+            print(f"Git pull stderr: {e.stderr}")
             return f"Error during webhook processing: {str(e)}", 500
     else:
         return "No action needed", 200
-    
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
